ea8d4a007639722896587b9034d591c882eaf694
Duane Bradbury <dibely@hotmail.com>
2017-06-23 22:37:27 +0100

    Added a dome reticle script ready for projecting an object onto a dome shape.

---
+++ Assets/Scripts/DomeReticle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DomeReticle : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObj;
+    public GameObject reticle;
+    private Vector3 hitPoint;
+
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hit;
+
+        if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100)) {
+            hitPoint = hit.point;
+            SetReticleToHitPoint(hit);
+        }
+    }
+
+    private void SetReticleToHitPoint(RaycastHit hit) {
+        reticle.transform.position = hitPoint;
+    }
+}
